d2dce8850acdc6f540a5eb386ba9bb18c2fb5012
pandu2406 <dinata240600@gmail.com>
2025-05-27 02:38:23 +0700

    gak sido6

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    experimental: {
+      serverComponentsExternalPackages: ['@prisma/client'],
+    },
+  };
   
-};
-
+  module.exports = nextConfig;
+  
 export default nextConfig;
